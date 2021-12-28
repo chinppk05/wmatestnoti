9e81bf59067d77b48d6bdcd2d6008557fda7e36b
chinppk05 <77804297+chinppk05@users.noreply.github.com>
2021-12-28 16:39:02 +0700

    Update basic01.py

--- basic01.py
+++ basic01.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*- 
 
 import requests
+from datetime import datetime
+
+now = datetime.now() # current date and time
+time = now.strftime("%H:%M:%S")
+
+
 url = 'https://notify-api.line.me/api/notify'
 token = 'Ryn034SAsglah4mmDvBKwz7Zwjx46BfWngmjJpnpmep'
 headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
@@ -10,7 +16,7 @@ headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'B
 #Ryn034SAsglah4mmDvBKwz7Zwjx46BfWngmjJpnpmep ชิน
 #3IHDRHBTUWBmhGRo6aLMEircbPQDUTfEflQQblrBHKL กลุ่ม
 eiei = '5555'
-msg = 'ทดสอบบอทสำหรับ Big Data'+' '+eiei + '\n\n' + 'บันทัดใหม่'
+msg = '\nทดสอบการแจ้งเตือน Big Data \nทุกๆ 1 ชั่วโมง \n'+'ขณะนี้เวลา '+ time +'\n\n' + 'Complete!!'
 
 r = requests.post(url, headers=headers, data = {'message':msg})
 print (r.text)
