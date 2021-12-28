b68e8632c227419165531cd4e20f1e8c2eacb4a7
chinppk05 <77804297+chinppk05@users.noreply.github.com>
2021-12-28 16:02:23 +0700

    Add files via upload

---
+++ basic01.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+import requests
+url = 'https://notify-api.line.me/api/notify'
+token = 'Ryn034SAsglah4mmDvBKwz7Zwjx46BfWngmjJpnpmep'
+headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+
+
+#Ryn034SAsglah4mmDvBKwz7Zwjx46BfWngmjJpnpmep ชิน
+#3IHDRHBTUWBmhGRo6aLMEircbPQDUTfEflQQblrBHKL กลุ่ม
+eiei = '5555'
+msg = 'ทดสอบบอทสำหรับ Big Data'+' '+eiei + '\n\n' + 'บันทัดใหม่'
+
+r = requests.post(url, headers=headers, data = {'message':msg})
+print (r.text)
